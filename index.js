25e3efd7c3797ad342b991f72a1bd3c97b724e72
Matheusmm097l <matheus.forcato.siqueira@escola.pr.gov.br>
2026-03-30 09:06:46 -0300

    fiz todos os operadores aritméticos e um de comparação

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-// run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+    //Operadores Aritméticos
+
+
+let number1 = 567;
+let number2 = 435;
+
+//soma
+
+let totalsoma = number1 + number2;
+console.log ('minha soma é: ',totalsoma);
+
+//subtração
+
+let number3 = 50
+let number4 = 25
+
+let totalsubtração = number3 - number4;
+console.log ('minha subtração é: ',totalsubtração);
+
+//multiplicação
+
+let number5 = 50
+let number6 = 5
+
+let totalmultiplicação = number5 * number6;
+console.log ('minha multiplicação é: ',totalmultiplicação);
+
+//divisão
+
+let number7 = 100
+let number8 = 4
+
+let totaldivisão = number7 / number8;
+console.log ('minha divisão é: ', totaldivisão);
+
+
+    //Operadores de Comparação
+
+let number9 = 10
+let number10 = 10
+
+let igualdade = number9 == number10;
+console.log ('a igualdade é: ', igualdade);
+
+
